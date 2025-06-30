390c1b29ea39014ff76eed643018cc85e64e423b
Vignesh Raghunathan <raghunathan@iiasa.ac.at>
2025-06-30 11:35:08 +0200

    fix(water_for_ppl): Incorrect MJ instead of GJ and /1e-6 division

--- message_ix_models/model/water/data/water_for_ppl.py
+++ message_ix_models/model/water/data/water_for_ppl.py
@@ -21,8 +21,9 @@ from message_ix_models.util import (
 
 log = logging.getLogger(__name__)
 
-# Convert m3/MJ to MCM/GWa
-m3_MJ_2_MCM_GWa = registry("m^3/MJ").to("m^3/GWa").magnitude
+# Convert m3/GJ to MCM/GWa
+m3_GJ_2_MCM_GWa = registry("m^3/GJ").to("m^3/GWa").magnitude / 1e-6
+# MCM not standard so have to remember to divide by 1e-6 each time.
 
 
 def missing_tech(x: pd.Series) -> pd.Series:
@@ -461,7 +462,7 @@ def cool_tech(context: "Context", scenario=None) -> dict[str, pd.DataFrame]:
     # this refers to activity per cooling requirement (heat)
     input_cool["value_cool"] = (
         input_cool["water_withdrawal_mid_m3_per_output"]
-        * m3_MJ_2_MCM_GWa  # conversion factor
+        * m3_GJ_2_MCM_GWa  # conversion factor
         / input_cool["cooling_fraction"]
     )
     # set to 1e-6 if value_cool is negative
@@ -1028,7 +1029,7 @@ def non_cooling_tec(context: "Context", scenario=None) -> dict[str, pd.DataFrame
     non_cool_df = non_cool_df.rename(columns={"technology_name": "technology"})
 
     non_cool_df["value"] = (
-        non_cool_df["water_withdrawal_mid_m3_per_output"] * m3_MJ_2_MCM_GWa
+        non_cool_df["water_withdrawal_mid_m3_per_output"] * m3_GJ_2_MCM_GWa
     )  # Conversion factor
 
     non_cool_tech = list(non_cool_df["technology"].unique())
